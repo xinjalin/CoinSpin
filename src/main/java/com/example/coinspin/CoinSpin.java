70f10c58c4e34116406b84b66f14e3e091d408c9
Christopher Osborn <xinjalin@gmail.com>
2022-05-29 22:04:06 +0930

    v1.0 Java FX using an array to loop through key images and display animation

--- src/main/java/com/example/coinspin/CoinSpin.java
+++ src/main/java/com/example/coinspin/CoinSpin.java
@@ -1,23 +1,58 @@
 package com.example.coinspin;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class CoinSpin extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage theStage)
+    {
+        theStage.setTitle("Coin Spin");
+
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(512,512);
+        root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image space = new Image("space.png");
+
+        AnimatedImage coin = new AnimatedImage();
+        Image[] imageArray = new Image[8];
+        for (int i = 0; i < 8; i++)
+        {
+            imageArray[i] = new Image("coin_" + i + ".png");
+        }
+
+        coin.frames = imageArray;
+        coin.duration = 0.175;
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer(){
+            public void handle(long currentNanoTime){
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                gc.drawImage(space, 0,0);
+
+                gc.drawImage(coin.getFrame(t), 125,125);
+            }
+        }.start();
+        theStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.example.coinspin {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-                requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-            requires com.almasb.fxgl.all;
-    
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
     opens com.example.coinspin to javafx.fxml;
     exports com.example.coinspin;
 }
