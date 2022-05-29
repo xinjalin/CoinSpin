4761393565de1737bfc8475a4276d8284668d927
Christopher Osborn <xinjalin@gmail.com>
2022-05-29 22:02:38 +0930

    Spinning Coin using stephen's coin img file's

---
+++ src/main/java/com/example/coinspin/AnimatedImage.java
@@ -0,0 +1,2 @@
+package com.example.coinspin;public class AnimatedImage {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.coinspin {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+                requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.coinspin to javafx.fxml;
+    exports com.example.coinspin;
+}
